48067b87be4f2a2de0386ba268cc8dea94f8bc6d
TienThanh <tien.thanh.info22@gmail.com>
2026-03-28 15:59:34 +0700

    fix: Update canInteract method to check for 'build' permission instead of 'use'

--- core/src/main/java/github/nighter/smartspawner/hooks/protections/api/Residence.java
+++ core/src/main/java/github/nighter/smartspawner/hooks/protections/api/Residence.java
@@ -14,7 +14,7 @@ public class Residence {
     }
 
     public static boolean canInteract(@NotNull Player player, @NotNull Location location) {
-        return check(player, location, "use");
+        return check(player, location, "build");
     }
 
     public static boolean canStack(@NotNull Player player, @NotNull Location location) {
