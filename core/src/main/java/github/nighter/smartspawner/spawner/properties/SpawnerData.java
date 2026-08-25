da278766d4d85be9043dfa8c381560b646b5b302
vincenzopio <vincenzopio03@icloud.com>
2026-08-25 18:19:18 +0200

    feat: identify an item spawner by its name, not by its material

--- core/src/main/java/github/nighter/smartspawner/spawner/properties/SpawnerData.java
+++ core/src/main/java/github/nighter/smartspawner/spawner/properties/SpawnerData.java
@@ -164,9 +164,10 @@ public class SpawnerData {
         this.spawnerId = id;
         this.spawnerLocation = location;
         this.entityType = EntityType.ITEM;
-        this.spawnedItemMaterial = itemMaterial;
-        this.configName = configName == null || configName.isBlank()
-                ? defaultItemName(plugin, itemMaterial) : configName;
+        var definition = plugin.getItemSpawnerSettingsConfig().getDefinition(configName);
+        this.spawnedItemMaterial = definition != null ? definition.material() : itemMaterial;
+        this.configName = definition != null ? definition.name()
+                : (configName == null || configName.isBlank() ? defaultItemName(plugin, itemMaterial) : configName);
 
         initializeDefaults();
         loadConfigurationValues();
@@ -198,9 +199,8 @@ public class SpawnerData {
 
         // Load loot config based on spawner type
         if (isItemSpawner() && spawnedItemMaterial != null) {
-            var definition = plugin.getItemSpawnerSettingsConfig().getDefinition(configName);
-            this.lootConfig = definition != null ? definition.lootConfig()
-                    : plugin.getItemSpawnerSettingsConfig().getLootConfig(spawnedItemMaterial);
+            this.lootConfig = plugin.getItemSpawnerSettingsConfig()
+                    .getLootConfig(configName, spawnedItemMaterial);
         } else {
             var definition = plugin.getSpawnerSettingsConfig().getDefinition(configName);
             this.lootConfig = definition != null ? definition.lootConfig()
@@ -406,7 +406,7 @@ public class SpawnerData {
 
     public void updateHologramData() {
         if (hologram != null) {
-            hologram.updateData(stackSize, entityType, spawnedItemMaterial, spawnerExp, maxStoredExp,
+            hologram.updateData(stackSize, entityType, spawnedItemMaterial, configName, spawnerExp, maxStoredExp,
                     virtualInventory.getUsedSlots(), maxSpawnerLootSlots);
         }
     }
@@ -509,9 +509,8 @@ public class SpawnerData {
     public void setLootConfig() {
         // Load loot config based on spawner type
         if (isItemSpawner() && spawnedItemMaterial != null) {
-            var definition = plugin.getItemSpawnerSettingsConfig().getDefinition(configName);
-            this.lootConfig = definition != null ? definition.lootConfig()
-                    : plugin.getItemSpawnerSettingsConfig().getLootConfig(spawnedItemMaterial);
+            this.lootConfig = plugin.getItemSpawnerSettingsConfig()
+                    .getLootConfig(configName, spawnedItemMaterial);
         } else {
             var definition = plugin.getSpawnerSettingsConfig().getDefinition(configName);
             this.lootConfig = definition != null ? definition.lootConfig()
